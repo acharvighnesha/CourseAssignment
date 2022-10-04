3b8f7fe760d7c79cfdcc1f991872be632408eeef
acharvighnesha <vighnesha.achar@swiggy.in>
2022-10-04 12:43:37 +0530

    changed private and public sg

---
+++ terraform/ec2-public.tf
@@ -0,0 +1,18 @@
+module "ec2_public" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "bastion"
+
+  ami                    = "ami-08c40ec9ead489470"
+  instance_type          = "t2.micro"
+  monitoring             = true
+  vpc_security_group_ids = [module.sg-ssh.security_group_id]
+  subnet_id              = module.vpc.public_subnets[1]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+    Name        = "bastion"
+  }
+}
